7b23e9c190428c49276545c065adbba65c71ea3d
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-11-25 16:14:36 +0300

    welcome page complete

--- SudokuSolver/screens/Homepage.js
+++ SudokuSolver/screens/Homepage.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+const myImage = require('../assets/logo.jpg');
+
 
 const HomePage = ({ navigation }) => {
   const handleStartPress = () => {
-    navigation.navigate('Solution');
+    navigation.navigate('Solver');
   };
 
   return (
     <View style={styles.container}>
+      <View>
+        <Image source={myImage} style={{ width: 200, height: 200, borderWidth:2, backgroundColor:'red' }} />
+      </View>
       <Text style={styles.welcomeText}>Welcome to Sudoku Solver</Text>
       <TouchableOpacity style={styles.startButton} onPress={handleStartPress}>
         <Text style={styles.startButtonText}>Start</Text>
@@ -21,16 +26,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor:'#fff',
   },
   welcomeText: {
+    marginTop: 30,
     fontSize: 24,
     marginBottom: 20,
   },
   startButton: {
-    backgroundColor: '#3498db',
+    backgroundColor: '#008d70',
     paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
+    paddingHorizontal: 90,
+    borderRadius: 10,
+    marginTop: 90,
   },
   startButtonText: {
     color: '#fff', 
